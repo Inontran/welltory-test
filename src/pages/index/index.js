0fa71886abc68b9efad4e1448539b38e1cfcd120
Inontran <pakim.wesi@gmail.com>
2021-11-26 12:21:09 +0900

    Сделана карточки выбора тарифа 'tariff-card'.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,5 +3,6 @@ import 'normalize.css';
 import '@/shared/theme/global.scss';
 import '@/components/header/header';
 import '@/components/footer/footer';
+import '@/components/tariff-card/tariff-card';
 
 import './index.scss';
