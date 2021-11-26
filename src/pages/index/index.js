436ae4d2a429415150d37ec6f1014eb8783851d4
Inontran <pakim.wesi@gmail.com>
2021-11-26 15:30:35 +0900

    Сделан новый блок 'button'.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,5 +4,6 @@ import '@/shared/theme/global.scss';
 import '@/components/header/header';
 import '@/components/footer/footer';
 import '@/components/tariff-card/tariff-card';
+import '@/components/button/button';
 
 import './index.scss';
