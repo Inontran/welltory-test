a6333336f50934163cf8d988e29e7f5fb5b32fd2
Inontran <pakim.wesi@gmail.com>
2021-11-26 16:49:27 +0900

    Сделан новый блок выбора тарифа 'choice-tariff'.

---
+++ src/components/choice-tariff/choice-tariff.js
@@ -0,0 +1,4 @@
+import '@/components/button/button';
+import '@/components/tariff-card/tariff-card';
+
+import './choice-tariff.scss';
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,7 +3,7 @@ import 'normalize.css';
 import '@/shared/theme/global.scss';
 import '@/components/header/header';
 import '@/components/footer/footer';
-import '@/components/tariff-card/tariff-card';
+import '@/components/choice-tariff/choice-tariff';
 import '@/components/button/button';
 
 import './index.scss';
